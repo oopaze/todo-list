5171af4c4f0f53f3a1144963f268f837c9a3984b
José  Pedro <jgomes@geobit.com.br>
2020-10-15 18:54:20 -0300

    heroku config

--- src/settings.py
+++ src/settings.py
@@ -77,19 +77,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': config('DATABASE_URL',
-        default = {
-            'NAME': os.environ.get('DB_NAME', 'todo-app'),
-            'USER': os.environ.get('DB_USER', 'root'),
-            'PASSWORD': os.environ.get('DB_PASSWD', '1234'),
-            'HOST': os.environ.get('DB_HOST', 'localhost'),
-            'PORT': os.environ.get('DB_PORT', '5432')
-        }
-    )
-}
-
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
