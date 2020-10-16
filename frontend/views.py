b72b8949c10ee51306bd9aaa72567126e024dbda
José  Pedro <jgomes@geobit.com.br>
2020-10-15 22:33:23 -0300

    agora vai

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 import ipdb
 
 def todo_list(request):
-    import ipdb; ipdb.set_trace()
     uri = request.get_raw_uri().split('?')[0]
     return render(request, 'frontend/list.html', {'uri':uri})
